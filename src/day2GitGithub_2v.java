724970bebc312890562ab4833c30632878003107
Alp Bulut <98495834+AlpBulutSoftware@users.noreply.github.com>
2022-08-19 02:27:12 +0300

    Update day2GitGithub_2v.java

--- src/day2GitGithub_2v.java
+++ src/day2GitGithub_2v.java
@@ -1,25 +1,33 @@
 public class day2GitGithub_2v {
 
-    /*
+/*
     1-) git init --> Local repo oluşturmak için yani .git ile klasörümüzün
-    içindeki dosyaları ilişkilendirmek için kullanılır
+        içindeki dosyaları ilişkilendirmek için kullanılır.
+    
     2-) git add . --> Working space'den (yani yerel) dosyalarımı staging area'ya
-    (yani commitlemek için beklenen yer) gönderir
-    3-) git status Working space'deki ve staging area'daki durumu gösterir
+        (yani commitlemek için beklenen yer) gönderir.
+    
+    3-) git status Working space'deki ve staging area'daki durumu gösterir.
+    
     4-) git commit -m "mesaj" --> Staging area'dan commit stora
-    dosyalarımı göndermek için kullanılır (commit yani versiyon
-    yani sürüm oluşturmuş olurum)
+        dosyalarımı göndermek için kullanılır (commit yani versiyon
+        yani sürüm oluşturmuş olurum).
+    
     5-) git push --> Uzak repo'ya(yani remote - Github)
-    göndermek için kullandığımız kod Yalnız git push komutunu direk kullanmak istersek 1 kez
+        göndermek için kullandığımız kod Yalnız git push komutunu direk kullanmak istersek 1 kez
             git remote add origin adres
             git push -u origin master
+            
     Not_1: Yukarıdaki iki komutu tek seferde kullandıktan sonra ikinci commit'lerim için sadece git push
-    kullanırız.
-    Not_2: git log --oneline --> commitlerimdeki yani versiyonlarımdaki durumu gösterir
-    Not_3: Github öğrendim 2v
-    Not_4: Github öğrendim version2
+           kullanırız.
+    Not_2: git log --oneline --> commitlerimdeki yani versiyonlarımdaki durumu gösterir.
+    Not_3: Github öğrendim 2v.
+    Not_4: Github öğrendim version2.
+    
     6-) git pull --> remote yani uzak repo'daki dosyalarımızı güncellemek için kullanılır.
-    Not_5: Github çok eğlenceli
+    
+    Not_5: Github çok eğlenceli.
+    Not_6: Github çok eğlenceli ama başlangıçta biraz yorucu.
 
 
  */
