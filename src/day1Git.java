639bd1f0af2874ba2e0ba2f1c06df0cdc9e1236b
Alp Bulut <alpbulut.software@gmail.com>
2022-08-20 21:08:30 +0300

    CVC

--- src/day1Git.java
+++ src/day1Git.java
@@ -5,5 +5,6 @@ public class day1Git {
         System.out.println("Git 1. Deneme");
         System.out.println("Git 2. Deneme");
         System.out.println("Git 3. Deneme");
+        System.out.println("Git 4. Deneme");
     }
 }
